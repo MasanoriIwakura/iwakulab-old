05d8b345b6e8de8d0b4d3fd1d11b18a68aafb25d
MasanoriIwakura <hagobutler@yahoo.co.jp>
2019-03-23 17:00:05 +0900

    スタイル変更
    ・ヘッダーを黒に変更(レスポンシブも)
    ・ロゴ変更

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "gatsby";
-import github from "../img/github-icon.svg";
-import logo from "../img/logo.svg";
+import github from "../img/github-icon-white.svg";
+import logo from "../img/iwakulab-logo.svg";
 
 const Navbar = class extends React.Component {
   constructor(props) {
@@ -42,7 +42,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: "88px" }} />
+              <img src={logo} alt="Iwakulab"/>
             </Link>
             {/* Hamburger menu */}
             <div
@@ -72,14 +72,11 @@ const Navbar = class extends React.Component {
               <Link className="navbar-item" to="/contact">
                 Contact
               </Link>
-              <Link className="navbar-item" to="/contact/examples">
-                Form Examples
-              </Link>
             </div>
             <div className="navbar-end has-text-centered">
               <a
                 className="navbar-item"
-                href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
+                href="https://github.com/MasanoriIwakura"
                 target="_blank"
                 rel="noopener noreferrer"
               >
